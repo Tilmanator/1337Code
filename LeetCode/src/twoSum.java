5f61454a946d1762a7d80ae4148eeddc5629a3f5
Tilman Lindig <tilman.lindig@rogers.com>
2018-02-10 17:42:29 -0800

    twoSum added

---
+++ LeetCode/src/twoSum.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class twoSum {
+	 public static int[] twoSumm(int[] nums, int target) {
+		 	int [] ret = new int[2];
+	        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+	        for(int i=0;i< nums.length; i++){
+	        	if(map.containsKey(nums[i])){
+	        		ret[1]=i;
+	        		ret[0]=map.get(nums[i]);
+	        	}
+	        	map.put((target-nums[i]), i);
+	        }
+	        return ret;
+	  }
+	 
+	 
+	 public static void main(String [] args){
+		 int [] test = {2,7,11,15};
+		 int tar = 9;
+		 int [] p = twoSumm(test,tar);
+		 for(int o:p)
+			 System.out.println(o);
+	 }
+}
