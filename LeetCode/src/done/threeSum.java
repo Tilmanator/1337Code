8d689a46405e38e27bbeddfa4ef50f8113750a95
Tilman Lindig <tilman.lindig@rogers.com>
2018-02-15 00:04:37 -0800

    Three sum accepted

--- LeetCode/src/done/threeSum.java
+++ LeetCode/src/done/threeSum.java
@@ -10,21 +10,35 @@ import java.util.Set;
 
 public class threeSum {
 
+	// Take advantage of the fact that given array is sorted
 	public static List<List<Integer>> modTwoSum(int[] nums, int target) {
 		List<List<Integer>> ret = new ArrayList<>();
-		Set<List<Integer>> p = new HashSet<>();
-		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-		for (int i = 0; i < nums.length; i++) {
-			// Skip duplicates
-			if (map.containsKey(nums[i])) {
-				ArrayList<Integer> temp = new ArrayList<Integer>(2);
-				temp.add(map.get(nums[i]));
-				temp.add(nums[i]);
-				p.add(temp);
+		int low = 0;
+		int high = nums.length-1;
+		
+		// Since sorted, simply move numbers inwards until we reach the target or exit condition
+		while(low < high){
+			// target reached, now skip any duplicates and keep looking
+			if(nums[low] + nums[high] == target){
+				ArrayList<Integer> tmp = new ArrayList<Integer>();
+				tmp.add(nums[low]);
+				tmp.add(nums[high]);
+				ret.add(tmp);
+				while(low < high && nums[low] == nums[low + 1])
+					++low;
+				while(low < high && nums[high] == nums[high-1])
+					--high;
+				++low;
+				--high;
 			}
-			map.put((target - nums[i]), nums[i]);
+			// below target so increase lower pointer, same concept for higher pointer
+			else if(nums[low] + nums[high] < target){
+				++low;
+			}
+			else{
+				--high;
+			}
 		}
-		ret.addAll(p);
 		return ret;
 	}
 
@@ -32,19 +46,20 @@ public class threeSum {
 		if (nums.length < 3)
 			return new ArrayList<>();
 		List<List<Integer>> ret = new ArrayList<>();
+		// Sorted now we can optimize two sum and skip duplicates easily
 		Arrays.sort(nums);
 		for (int i = 0; i < nums.length - 2; ++i) {
+			// Skip duplicates (they can still occur in solution because we start with the first one)
 			if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
-				int[] n = new int[nums.length - i - 1];
-				for (int j = i + 1; j < nums.length; ++j) {
-					n[j - (i + 1)] = nums[j];
+				int [] n = new int[nums.length-1-i];
+				for(int j=i+1; j< nums.length;++j){
+					n[j-(i+1)] = nums[j];
 				}
-				List<List<Integer>> r = modTwoSum(n, -nums[i]);
-				for (List<Integer> p : r) {
-					//List<Integer> tmp = (List<Integer>) ((ArrayList<Integer>) p).clone();
-					p.add(nums[i]);
-					p.sort(null);
-					ret.add(p);
+				// Perform twoSum with the target being negative current number
+				List<List<Integer>> tmp = modTwoSum(n,-nums[i]);
+				for(List<Integer> a: tmp){
+					a.add(nums[i]);
+					ret.add(a);
 				}
 			}
 		}
@@ -55,9 +70,9 @@ public class threeSum {
 	public static void main(String[] args) {
 		//int [] n = {1,2,-3,4,5,6,-7,8,9,10};
 		int [] n = {-2,-2,-2,-2,-2,-2,2,2,4,-1,-1,0,0,1,1};
-		int[] p = { 0,0,0 };
+		int[] p = {-1,0,1,2,-1,-4};
 		// int tar = 10;
-		List<List<Integer>> ret = threeSumm(n);
+		List<List<Integer>> ret = threeSumm(p);
 		System.out.println(ret.size());
 		for (List<Integer> i : ret) {
 			System.out.println(i.get(0) + " " + i.get(1) + " " + i.get(2));
