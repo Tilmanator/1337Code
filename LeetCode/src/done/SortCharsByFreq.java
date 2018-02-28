9cd1ad5d60755b0edb5cc49334fbc425c23ebe94
Tilman Lindig <tilman.lindig@rogers.com>
2018-02-27 19:14:15 -0800

    Completed sort chars by freq

--- LeetCode/src/done/SortCharsByFreq.java
+++ LeetCode/src/done/SortCharsByFreq.java
@@ -1,25 +1,12 @@
-import java.util.Comparator;
+package done;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Map;
+import java.util.List;
 import java.util.Map.Entry;
-import java.util.SortedSet;
-import java.util.TreeSet;
 
 public class SortCharsByFreq {
-
-	// Sort by values descending
-	static <K, V extends Comparable<? super V>> SortedSet<Map.Entry<K, V>> customEntriesSortedByValues(Map<K, V> map) {
-		SortedSet<Map.Entry<K, V>> sortedEntries = new TreeSet<Map.Entry<K, V>>(new Comparator<Map.Entry<K, V>>() {
-			@Override
-			public int compare(Map.Entry<K, V> e1, Map.Entry<K, V> e2) {
-				int res = e1.getValue().compareTo(e2.getValue());
-				return res != 0 ? -res : -1;
-			}
-		});
-		sortedEntries.addAll(map.entrySet());
-		return sortedEntries;
-	}
 
 	public static String frequencySort(String s) {
 		// Keep track of number of occurrences per char
@@ -28,25 +15,44 @@ public class SortCharsByFreq {
 			char c = s.charAt(i);
 			m.put(c, 1 + (m.containsKey(c) ? m.get(c) : 0));
 		}
-		String ret = "";
 
-		// Sort properly
-		SortedSet<Map.Entry<Character, Integer>> q = customEntriesSortedByValues(m);
-		Iterator<Entry<Character, Integer>> it = q.iterator();
+		// Initialize list of lists of characters
+		// The reason is that we can just go backwards through this list as
+		// we will add a character at the index = it's count
+		// Traversing it backwards thus gives us chars in order of their count
+		List<ArrayList<Character>> list = new ArrayList<ArrayList<Character>>(s.length() + 1);
+		for (int i = 0; i < s.length() + 1; i++)
+			list.add(i, new ArrayList<Character>());
 
-		// Make the string based on sorted values
+		// Append the character to the list at an index = it's count
+		// This is why we need list of list:
+		// Different chars can have same count
+		// (otherwise we could use reversed TreeMap)
+		Iterator<Entry<Character, Integer>> it = m.entrySet().iterator();
 		while (it.hasNext()) {
 			Entry<Character, Integer> e = it.next();
+
 			for (int i = 0; i < e.getValue(); ++i) {
-				ret += e.getKey();
+				list.get(e.getValue()).add(e.getKey());
 			}
 		}
 
-		return ret;
+		// Stringbuilder is better performance than string
+		StringBuilder ret = new StringBuilder("");
+		// Start from most occurrences and work down
+		for (int i = list.size() - 1; i >= 0; i--) {
+			if (list.get(i).size() > 0) {
+				ArrayList<Character> temp = list.get(i);
+				for (int j = 0; j < temp.size(); ++j)
+					ret.append(temp.get(j));
+			}
+		}
+
+		return ret.toString();
 	}
 
 	public static void main(String[] args) {
-		System.out.println(frequencySort("poasdjjeeavvv"));
+		System.out.println(frequencySort("poasdAjjeeavvv"));
 
 	}
 
