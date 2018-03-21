dfb8305078a037d1ffa57606b93f77cadac0f547
Tilman Lindig <tilman.lindig@rogers.com>
2018-03-21 01:17:21 -0700

    Improved solution for min arrows to burst balloons

--- LeetCode/src/done/MinimumNumberOfArrowsToBurstBalloons.java
+++ LeetCode/src/done/MinimumNumberOfArrowsToBurstBalloons.java
@@ -1,49 +1,36 @@
 package done;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 
+// Old way was with Tuples, extending the range...can avoid this by sorting on second value OR by iterating through it backwards
 public class MinimumNumberOfArrowsToBurstBalloons {
 	public int findMinArrowShots(int[][] points) {
 		if (points.length == 0)
 			return 0;
 
-		// Sort the input array to ensure we don't miss solutions due to the
-		// order of balloons (ex: 2:3, 3:4, 1:2, 4:5 needs 2 not 3)
+		// Sort array by second value to iterate through it from front to end,
+		// tracking only the last value of the previous range
 		Arrays.sort(points, new Comparator<int[]>() {
 			public int compare(int[] a, int[] b) {
-				return Integer.compare(a[0], b[0]);
+				return Integer.compare(a[1], b[1]);
 			}
 		});
 
-		// Keep track of what we have
-		List<Tuple> range = new ArrayList<Tuple>();
-		range.add(new Tuple(points[0][0], points[0][1]));
+		int arrows = 1;
+		int lastEnd = points[0][1];
+
 		for (int i = 1; i < points.length; ++i) {
-			// The new point fits in the range of the old one
-			if (points[i][0] <= range.get(range.size() - 1).y) {
-				// The new x is guaranteed to be >=
-				range.get(range.size() - 1).x = points[i][0];
-				// Reduce the range if necessary
-				range.get(range.size() - 1).y = Math.min(points[i][1], range.get(range.size() - 1).y);
-			} else {
-				range.add(new Tuple(points[i][0], points[i][1]));
-			}
+			// Fits within previous range and we know its end is larger
+			if (points[i][0] <= lastEnd)
+				continue;
+
+			// Otherwise we need to start a new range
+			++arrows;
+			lastEnd = points[i][1];
 		}
 
 		// Arrows can be fired anywhere in the ranges
-		return range.size();
-	}
-}
-
-class Tuple {
-	int x;
-	int y;
-
-	Tuple(int a, int b) {
-		x = a;
-		y = b;
+		return arrows;
 	}
 }
